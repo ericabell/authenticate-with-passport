c3508c2cccd3607bd550ab5ab1e73b020c6fb2fa
Eric Abell <ericabell@users.noreply.github.com>
2017-08-23 20:06:34 -0500

    Finished basic example using bcrypt

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ console.log(`The password is: ${password}`);
 const hash = bcrypt.hashSync(password, 8);
 
 console.log(`The hashed password is ${hash}`);
+
+// we can compare whether a new string matches
+console.log(bcrypt.compareSync(password, hash));
